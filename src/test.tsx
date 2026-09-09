ffd499e126af32950aa849967536cfa71db4f8c0
SmartCodeAI <smartcodeaia@gmail.com>
2026-09-09 13:35:17 +0500

    change app component

---
+++ src/test.tsx
@@ -0,0 +1,16 @@
+console.log("Started Typescript Toturials on Date: 09 - 09 - 2026");
+
+// Type safety
+interface interfaceOfObject {
+  name: string;
+  id: number;
+  status: boolean;
+}
+
+const realObject: interfaceOfObject = {
+  name: "shahid",
+  id: 29,
+  status: true,
+};
+
+console.log(realObject);
